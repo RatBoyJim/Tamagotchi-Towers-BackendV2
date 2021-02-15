c04fe1a8ed8af034be5e7419a150003cf664feba
RatBoyJim <>
2021-02-15 11:01:35 +0000

    working

--- src/main/java/com/example/Tamagotchi_Towers/controllers/AnimalController.java
+++ src/main/java/com/example/Tamagotchi_Towers/controllers/AnimalController.java
@@ -8,10 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+//@RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
+//// For simplicity of this sample, allow all origins. Real applications should configure CORS for their use case.
+//@CrossOrigin(origins = "*")
 public class AnimalController {
 
     @Autowired
